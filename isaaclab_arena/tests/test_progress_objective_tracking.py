8e5a7af17a1df91b7375719a64163a9805ac30b2
Clemens Volk <cvolk@nvidia.com>
2026-09-17 14:39:04 +0200

    Address task termination review feedback
    Signed-off-by: Clemens Volk <cvolk@nvidia.com>

--- isaaclab_arena/tests/test_progress_objective_tracking.py
+++ isaaclab_arena/tests/test_progress_objective_tracking.py
@@ -47,6 +47,10 @@ class _MockEnv:
         self._object_initial_rest_pose_recorder = ObjectInitialRestPoseRecorder(num_envs, device)
 
     @property
+    def progress_tracker(self):
+        return self._progress_tracker
+
+    @property
     def object_initial_rest_pose_recorder(self):
         return self._object_initial_rest_pose_recorder
 
@@ -745,7 +749,7 @@ def _test_recorder_publishes_to_extras_and_records_nothing(simulation_app) -> bo
     objectives = [ProgressObjective(name="task", predicate_sequence=[first_predicate, final_predicate])]
     recorder_cfg = ProgressTrackingRecorderCfg()
     recorder = recorder_cfg.class_type(recorder_cfg, env)
-    assert env._progress_tracker is None
+    assert env.progress_tracker is None
     success_cfg = TerminationTermCfg(func=TaskSuccessTerm, params={"success_objectives": objectives})
     success = TaskSuccessTerm(success_cfg, env)
 
@@ -789,7 +793,7 @@ def _test_task_termination_cfg_contains_success_objectives(simulation_app) -> bo
     default_cfg = default_task.get_termination_cfg()
     assert isinstance(default_cfg, TaskTerminationCfg)
     assert default_cfg.success == []
-    assert default_cfg.timeout_s == default_task.episode_length_s
+    assert default_cfg.timeout_s is None
 
     objective = ProgressObjective(name="lift", predicate_sequence=[_MockPredicate(num_envs=1, name="lift")])
 
